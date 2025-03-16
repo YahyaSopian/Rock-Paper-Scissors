96a4270b2412e2bf7f60471c3a3cbfc419e3d27b
Yahya Sopian <yahyasopian5@gmail.com>
2025-03-16 14:17:25 +0700

    Adding div for displaying result Adding event listener to button using DOM Displaying result in HTML for every winner and remove console.logs statement

--- main.js
+++ main.js
@@ -16,17 +16,36 @@ function playGame() {
   function playRound(humanChoice, computerChoice) {
     humanChoice = humanChoice.toLowerCase();
 
+    // mengakses div dengan id result untuk menampilkan hasil
+    const result = document.getElementById("result");
     // Cek pemenang
     if (humanChoice === computerChoice) {
+      result.textContent = "Seri!";
       console.log(`Seri!`);
     } else if ((humanChoice === `rock` && computerChoice === `scissors`) || (humanChoice === `paper` && computerChoice === `rock`) || (humanChoice === `scissors` && computerChoice === `paper`)) {
+      result.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
       console.log(`You win! ${humanChoice} beats ${computerChoice}`);
       humanScore++;
     } else {
+      result.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
       console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
     }
     computerScore++;
   }
+
+  // Mengakses tombol pilihan dan menambahkan event listener
+  document.getElementById("rock").addEventListener("click", () => {
+    const computerChoice = getComputerChoice();
+    playRound("rock", computerChoice);
+  });
+  document.getElementById("paper").addEventListener("click", () => {
+    const computerChoice = getComputerChoice();
+    playRound("paper", computerChoice);
+  });
+  document.getElementById("scissors").addEventListener("click", () => {
+    const computerChoice = getComputerChoice();
+    playRound("scissors", computerChoice);
+  });
 }
 
 playGame();
